b878a9e92fd13d57968f57922b007a22b160dc05
root <root@localhost.localdomain>
2025-06-16 22:02:13 -0400

    Initial commit. Contains main script(still in progress), readme, and gitignore. More to come...

---
+++ selinux_lab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#This script will create selinux issues in three difficulties; easy, normal and hard mode. It is a script made to create labs for selinux practice. Although there are only three potential issues, repeating these tasks will enhance overall selinux proficiency.
+
+echo "welcome"
